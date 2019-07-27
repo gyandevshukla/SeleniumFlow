17c09dbdd0fef13ce252759d368d1691611a9ebf
gyandev shukla <gyandev.shukla@gmail.com>
2019-07-28 02:00:00 +0530

    added

--- com.test.seleniumFlow/src/test/java/com/test/seleniumFlow/BaseTest.java
+++ com.test.seleniumFlow/src/test/java/com/test/seleniumFlow/BaseTest.java
@@ -6,11 +6,10 @@ import org.testng.annotations.Test;
 
 public class BaseTest {
 	@Test
-	public void base() throws InterruptedException{
+	public void base(){
 		System.setProperty("webdriver.chrome.driver", "src/main/java/chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
-		driver.get("https://www.irctc.co.in");
-		Thread.sleep(100);
+		driver.get("https://www.seleniumeasy.com/test/");
         driver.close();
         
 	}
